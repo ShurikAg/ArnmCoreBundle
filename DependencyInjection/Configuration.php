488260ce8f2cc1cc9a89870f16e8a2add8a6c920
Alex Agulyansky <alex@iibspro.com>
2012-09-26 01:32:49 -0700

    - Removed frontend stuff - PHPMD fixes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace Arnm\CoreBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
-
 /**
  * This is the class that validates and merges configuration from your app/config files
  *
@@ -18,11 +16,13 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('arnm_core');
+        //$rootNode =
+        $treeBuilder->root('arnm_core');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
+
 
         return $treeBuilder;
     }
