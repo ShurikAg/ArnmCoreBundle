cef17ee76a8ad88f6de03119024f03b38824242c
Alex Agulyansky <alex@iibspro.com>
2013-02-28 01:58:59 -0800

    Exposed collector as a service

--- DependencyInjection/ArnmCoreExtension.php
+++ DependencyInjection/ArnmCoreExtension.php
@@ -25,7 +25,7 @@ class ArnmCoreExtension extends Extension
         //$config =
         $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
